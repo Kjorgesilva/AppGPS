df0dcfe51e95fd556b93e187fbef5f518ea8c769
Kjorgesilva <kaiqueadm@hotmail.com>
2019-06-28 17:07:23 -0300

    Atualizacao do app

--- app/src/main/java/com/example/appgps/ForeGround/MyForeGroundService.java
+++ app/src/main/java/com/example/appgps/ForeGround/MyForeGroundService.java
@@ -1,160 +1,223 @@
 package com.example.appgps.ForeGround;
 
-import android.annotation.SuppressLint;
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
-import android.widget.Toast;
-import com.example.appgps.R;
 
+import com.example.appgps.MainActivity;
+import com.example.appgps.R;
+import com.example.appgps.Verificar;
+import com.example.appgps.WebService.LocalizacaoAtualWs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.support.v4.app.NotificationCompat.PRIORITY_MAX;
 
 public class MyForeGroundService extends Service {
-    private static final String TAG_FOREGROUND_SERVICE = "FOREGROUND_SERVICE";
-
     public static final String ACTION_START_FOREGROUND_SERVICE = "ACTION_START_FOREGROUND_SERVICE";
 
-    public static final String ACTION_STOP_FOREGROUND_SERVICE = "ACTION_STOP_FOREGROUND_SERVICE";
-
-    public static final String ACTION_PAUSE = "ACTION_PAUSE";
-
-    public static final String ACTION_PLAY = "ACTION_PLAY";
 
     public boolean ativo = true;
     public int count = 0;
-
-
+    LocationManager locationManager;
+    String lattitude, longitude;
+    public Context contexto = this;
+    Verificar verificar = new Verificar();
 
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
+
     @Override
     public void onCreate() {
+
         super.onCreate();
     }
 
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if(intent != null)
-        {
+        if (intent != null) {
             String action = intent.getAction();
-
-            switch (action){
+            switch (action) {
                 case ACTION_START_FOREGROUND_SERVICE:
                     startForegroundService();
-                    Toast.makeText(getApplicationContext(), "Ligado", Toast.LENGTH_LONG).show();
+
+                    // Toast.makeText(getApplicationContext(), "Ligado", Toast.LENGTH_LONG).show();
                     MyForeGroundService.Worke w = new Worke(startId);
                     w.start();
-                    break;
-                case ACTION_STOP_FOREGROUND_SERVICE:
-                    stopForegroundService();
-                    ativo = false;
-                    count = 0;
-                    Toast.makeText(getApplicationContext(), "Desligado", Toast.LENGTH_LONG).show();
                     break;
             }
         }
         return super.onStartCommand(intent, flags, startId);
     }
 
-    /* Used to build and start foreground service. */
-    private void startForegroundService()
-    {
-        // Create notification default intent.
+    public Notification getNotification() {
+        String channel;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            channel = createChannel();
+        else {
+            channel = "";
+        }
+
         Intent intent = new Intent();
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
-
-        // Create notification builder.
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-
-
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channel);
         builder.setWhen(System.currentTimeMillis());
-        builder.setSmallIcon(R.mipmap.ic_launcher);
-//        Bitmap largeIconBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_foreground);
-//        builder.setLargeIcon(largeIconBitmap);
+        builder.setSmallIcon(R.drawable.ic_simbolo_logo_sighra_color);
         builder.setSubText("Localização");
-        // Make the notification max priority.
-        builder.setPriority(Notification.PRIORITY_MAX);
 
-        // Make notification show big text.
+        // Texto do card
         NotificationCompat.BigTextStyle bigTextStyle = new NotificationCompat.BigTextStyle();
         bigTextStyle.setBigContentTitle("SIGhRA");
         bigTextStyle.bigText("Mensagem");
-        // Set big text style.
         builder.setStyle(bigTextStyle);
-
-
-        // Make head-up notification.
         builder.setFullScreenIntent(pendingIntent, true);
 
-//         Add Play button intent in notification.
-//        Intent playIntent = new Intent(this, MyForeGroundService.class);
-//        playIntent.setAction(ACTION_PLAY);
-//        PendingIntent pendingPlayIntent = PendingIntent.getService(this, 0, playIntent, 0);
-//        NotificationCompat.Action playAction = new NotificationCompat.Action(android.R.drawable.ic_media_play, "", pendingPlayIntent);
-//        builder.addAction(playAction);
-
-//        // Add Pause button intent in notification.
-//        Intent pauseIntent = new Intent(this, MyForeGroundService.class);
-//        pauseIntent.setAction(ACTION_PAUSE);
-//        PendingIntent pendingPrevIntent = PendingIntent.getService(this, 0, pauseIntent, 0);
-//        NotificationCompat.Action prevAction = new NotificationCompat.Action(android.R.drawable.ic_media_pause, "Pause", pendingPrevIntent);
-//        builder.addAction(prevAction);
-
-        // Build the notification.
-        Notification notification = builder.build();
-
+        Notification notification = builder.setPriority(PRIORITY_MAX).setCategory(Notification.CATEGORY_SERVICE).build();
         // Start foreground service.
         startForeground(1, notification);
+
+        return notification;
     }
 
-    private void stopForegroundService()
-    {
-        // Stop foreground service and remove the notification.
-        stopForeground(true);
+    @NonNull
+    @TargetApi(26)
+    private synchronized String createChannel() {
+        NotificationManager mNotificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        // Stop the foreground service.
-        stopSelf();
+        String name = "snap map fake location ";
+        int importance = NotificationManager.IMPORTANCE_LOW;
+
+        NotificationChannel mChannel = new NotificationChannel("snap map channel", name, importance);
+
+        mChannel.enableLights(true);
+        mChannel.setLightColor(Color.BLUE);
+        if (mNotificationManager != null) {
+            mNotificationManager.createNotificationChannel(mChannel);
+        } else {
+            stopSelf();
+        }
+        return "snap map channel";
     }
 
-  class Worke extends Thread {
+    private void startForegroundService() {
+        getNotification();
+    }
+
+
+    class Worke extends Thread {
         public int startId = 0;
 
         public Worke(int startId) {
             this.startId = startId;
         }
 
-        @SuppressLint("MissingPermission")
         public void run() {
-
             while (ativo) {
-                //Log.e("contador", "antes try ");
-                try {
 
+                try {
                     Thread.sleep(1000);
+
+                    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                    if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                        Log.e("localizacao", "localização esta off");
+
+                        //verificar.requestPermission();
+
+                    } else if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                        getLocation();
+                    }
+
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                count++;
-                Log.e("contador", "contador: " + count);
-//                Looper.prepare();
-//                Toast.makeText(getApplicationContext(), "contador" + count, Toast.LENGTH_SHORT).show();
-//                Toast.makeText(getApplicationContext(), "ativo: " + ativo, Toast.LENGTH_SHORT).show();
-//
-//                Log.e("contador", "antes do loop ");
-//                Looper.loop();
-//                Looper.getMainLooper().quit();
-//                Log.e("contador", "depois loop ");
-
 
             }
         }
 
     }
+
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(contexto, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
+                (contexto, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+        } else {
+            //GPS
+            Location locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            //REDE
+            Location locationREDE = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            Location location2 = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+
+            if (locationGPS != null) {
+                double latti = locationGPS.getLatitude();
+                double longi = locationGPS.getLongitude();
+                lattitude = String.valueOf(latti);
+                longitude = String.valueOf(longi);
+
+                Log.e("localização", "Lattitude = " + lattitude
+                        + "\n" + "Longitude = " + longitude);
+                // enviaValor(latti, longi);
+
+            } else if (locationREDE != null) {
+                double latti = locationREDE.getLatitude();
+                double longi = locationREDE.getLongitude();
+                lattitude = String.valueOf(latti);
+                longitude = String.valueOf(longi);
+
+                Log.e("localização", "Lattitude = " + lattitude
+                        + "\n" + "Longitude = " + longitude);
+                // enviaValor(latti, longi);
+
+
+            } else if (location2 != null) {
+                double latti = location2.getLatitude();
+                double longi = location2.getLongitude();
+                lattitude = String.valueOf(latti);
+                longitude = String.valueOf(longi);
+
+                Log.e("localização", "Lattitude = " + lattitude
+                        + "\n" + "Longitude = " + longitude);
+
+            } else {
+
+                Log.e("log", "Não deu para rastrear");
+
+            }
+        }
+    }
+
+
+    private void enviaValor(Double latitude, Double longitude) {
+        Map<String, Double> map = new HashMap<>();
+        map.put("latitude", latitude);
+        map.put("longitude", longitude);
+        LocalizacaoAtualWs.enviarLocalizacao(contexto, "localizacao", map);
+
+    }
+
+
 }
+
+
+
--- app/src/main/java/com/example/appgps/MainActivity.java
+++ app/src/main/java/com/example/appgps/MainActivity.java
@@ -1,104 +1,149 @@
 package com.example.appgps;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
-import android.app.AlarmManager;
-import android.app.Notification;
-import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
-import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Build;
-import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.example.appgps.BroadcastReceiver.BroadcastReceiverTeste;
-import com.example.appgps.Service.TesteService;
+
+import com.example.appgps.ForeGround.MyForeGroundService;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
     private TextView localizacao;
     private LocationManager locationManager;
     private LocationListener listener;
-    private Context context = this;
+    private Context contexto = this;
     private Button botao;
+
+    private static final int REQUEST_CHECK_SETTINGS = 214;
+    private static final int REQUEST_ENABLE_GPS = 516;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startService(new Intent(getBaseContext(), TesteService.class));
 
-//        localizacao = findViewById(R.id.textView);
-//        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-//
-//        Intent intent = new Intent(getApplicationContext(), BroadcastReceiverTeste.class);
-//        intent.setAction("LOCALIZACAO_ATUAL");
+        Intent intent = new Intent(contexto, MyForeGroundService.class);
+        intent.setAction(MyForeGroundService.ACTION_START_FOREGROUND_SERVICE);
+        startService(intent);
 
+        Verificar verificar = new Verificar();
 
-        botao = findViewById(R.id.button);
-
-
-        botao.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent inte = new Intent(new Intent(context, CreateForegroundServiceActivity.class));
-                startActivity(inte);
-
-            }
-        });
-
-//
-//
-//        LocationListener listener = new LocationListener() {
-//            @SuppressLint("SetTextI18n")
-//            @Override
-//            public void onLocationChanged(Location location) {
-//               localizacao.setText("\n " + location.getLongitude() + " " + location.getLatitude());
-//            }
-//
-//            @Override
-//            public void onStatusChanged(String s, int i, Bundle bundle) {
-//
-//            }
-//
-//            @Override
-//            public void onProviderEnabled(String s) {
-//
-//            }
-//
-//            @Override
-//            public void onProviderDisabled(String s) {
-////                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-////                startActivity(i);
-//            }
-//        };
-//                   // configure_button();
-//            }
-//
-//
-//    void configure_button(){
-//        // first check for permissions
-//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
-//                        ,10);
-//            }
-//            return;
-//        }
-//
-//        locationManager.requestLocationUpdates("gps", 5000, 0, listener);
-//    }
+        //verifica se a permissão de localização foi aceita ou recusada
+        if (verificaPermissaoLocalizacao()) {
+            //se a permissão de localização foi aceita, chama o método que verifica o status (ligada ou desligada) de localização do sistema
+            verificaStatusLocalizacao();
+        } else {
+            //se a permissão de localização não foi aceita, chama o método que requisita permissão novamente
+            verificar.requestPermission();
+        }
 
 
-    }}
+    }
+    public boolean verificaPermissaoLocalizacao() {
+        if (ContextCompat.checkSelfPermission(contexto,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(contexto,
+                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+
+            return true;
+        } else {
+
+
+            return false;
+        }
+    }
+
+
+    public void verificaStatusLocalizacao() {
+
+        //verifica se a localização do dispositivo está ligada ou desligada
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(new LocationRequest().setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY));
+        builder.setAlwaysShow(true);
+        LocationSettingsRequest mLocationSettingsRequest = builder.build();
+
+        SettingsClient mSettingsClient = LocationServices.getSettingsClient(MainActivity.this);
+
+        mSettingsClient
+                .checkLocationSettings(mLocationSettingsRequest)
+                .addOnSuccessListener(new OnSuccessListener<LocationSettingsResponse>() {
+                    @Override
+                    public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                        //quando a localização estiver ligada, entra neste método
+                        Log.e("GPS", "success.");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // quando a localização ainda não estiver ligada, entra neste método
+                        int statusCode = ((ApiException) e).getStatusCode();
+                        switch (statusCode) {
+                            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                try {
+
+                                    ResolvableApiException rae = (ResolvableApiException) e;
+
+                                    // para chamar o onActivityResult em uma activity
+                                    // rae.startResolutionForResult(getActivity(), REQUEST_CHECK_SETTINGS);
+
+                                    // para chamar o onActivityResult em um fragment
+                                    // rae.getResolution().getIntentSender() = exibe um dialog solicitando para que o usuário ative a localização
+                                    // startIntentSenderForResult chama o onActivityResult para capturar a resposta da interação do usuário com o
+                                    // dialog exibido pelo rae.getResolution().getIntentSender()
+                                    startIntentSenderForResult(rae.getResolution().getIntentSender(), REQUEST_CHECK_SETTINGS, null,
+                                            0, 0, 0, null);
+
+                                    Log.e("GPS", "solicitou que usuário ative a localização");
+
+                                } catch (IntentSender.SendIntentException sie) {
+                                    Log.e("GPS", "Não foi possível executar a ação do dialog.");
+                                }
+                                break;
+
+                            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                                Log.e("GPS", "Location settings are inadequate, and cannot be fixed here. Fix in Settings.");
+                                break;
+                        }
+                    }
+                })
+                .addOnCanceledListener(new OnCanceledListener() {
+                    @Override
+                    public void onCanceled() {
+                        Log.e("GPS", "checkLocationSettings -> onCanceled");
+                    }
+                });
+    }
+
+
+
+}
